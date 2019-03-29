dfd25896cf78b5e3b99755a253c00c4c34ef582a
Sami Austin <samicaustin@gmail.com>
2019-03-28 22:12:13 -0600

    Chapter 10 - IN PROGRESS - Boromir needs to be struck through

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -150,12 +150,13 @@ const theBalrog = () => {
 // Chapter 10
 // ============
 const hornOfGondor = () => {
-
-  // 1. create a pop-up alert that the horn of gondor has been blown
-
+  alert("The Horn of Gondor has been blown!");
+  
   // 2. Boromir's been killed by the Uruk-hai! Put a linethrough on Boromir's name
+  $('li.buddy').eq(4).css("text-decoration", "line-through"); // ==> Not working, not sure why not
 
-  // 3. Tricky: Remove the Uruk-Hai from the Baddies on the page
+  $('li.baddy').eq(2).remove();
+  
 
 };
 
