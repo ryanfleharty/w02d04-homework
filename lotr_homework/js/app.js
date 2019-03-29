f886cba6c04d2fddc5242cfcb5121689accf8ee4
Alexandra Thornton <alexandrathornton@Alexandras-MacBook-Pro.local>
2019-03-28 19:22:21 -0600

    Chapter 9 complete - Updated Gandalf

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -207,8 +207,10 @@ $('.buddyPal').appendTo('#the-fellowship');
 const theBalrog = () => {
 
   // 1. change the 'Gandalf' textNode to 'Gandalf the White'
+  $('.buddy').eq(0).text("Gandalf the White");
 
   // 2. add a class "the-white" to this element
+  $('.buddy').eq(0).addClass("the-white");
 
   // 3. in the style.css file, add a css rule to make elements of the class "the-white" have a white background and a grey border
 
