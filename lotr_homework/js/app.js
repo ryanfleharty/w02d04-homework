e47951c67d7fe4778b3f88dc2960b9cfea8d77a2
WillyD <sopwill3@gmail.com>
2019-03-28 21:28:35 -0600

    gollum is in mount doom

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -272,11 +272,11 @@ const itsDangerousToGoAlone = () => {
 const weWantsIt = () => {
 
   // 1. Create a div with an id of 'gollum' and add it to Mordor
-
+  $('#Mordor').append(`<div id="gollum"></div>`);
   // 2. Move the ring from Frodo and give it to Gollum
-
+  $('#the-ring').detach().appendTo('#gollum')
   // 3. Move Gollum into Mount Doom
-
+  $('#gollum').detach().appendTo('#mount-doom');
 };
 
 // COMMIT YOUR WORK
