9583cd92cffdae65d17340000df62b20b734337c
John Oro <johnmichaeloro@gmail.com>
2019-03-29 09:51:45 -0600

    Chapter 1 complete - Made Middle Earth

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -44,17 +44,16 @@ const makeMiddleEarth = () => {
   // HINT: Make a console.log for each of your functions to make sure that, when you click, the correct function is being called!
 
   console.log("Trying to make middle earth.");
-
+  $('body').append('<section id="middle-earth"></section>');
   // 1. create a section tag with an id of middle-earth
-  const $middleEarth = $('<section id="middleEarth"></section>');
-  console.log($middleEarth);
+
   // 2. append the section to the body of the DOM.
-  $('body').append($middleEarth);
   // 3. use a for loop to iterate over the lands array that does the following:
-  for(let i = 0; i < 1; i++) {
-    lands[0] = $middleEarth.append('<article id="The-Shire"><h1>The-Shire</h1></article>');
-    lands[1] = $middleEarth.append('<article id="Rivendell"><h1>Rivendell</h1></article>');
-    lands[2] = $middleEarth.append('<article id="Mordor"><h1>Mordor</h1></article>');
+  for(let i = 0; i < lands.length; i++) {
+    $('#middle-earth').append('<article id= "cat"></article>');
+    $('#cat').append('<h1 id="landsh1"></h1>');
+    $('#landsh1').text(lands[i]);
+    $('#cat').attr('id', lands[i]);
   }
   //   3a. creates an article tag (there should be one for each land when the loop is done)
 
@@ -63,8 +62,7 @@ const makeMiddleEarth = () => {
   //   3c. includes an h1 with the name of the land inside each land article
 
   //   3d. appends each land to the middle-earth section
-
-};
+}
 
 makeMiddleEarth();
 
@@ -79,9 +77,12 @@ const makeHobbits = () => {
   console.log('Make hobbits');
 
   // 1. display an unordered list of the hobbits in the shire.
-
+  const $hobbitList = $('<ul><ul/>');
+  for (let i = 0; i < hobbits.length; i++) {
+    hobbits[i]
+  }
   // 2. give each hobbit a class of "hobbit"
-
+  $hobbitList.append()
   // hint: create a 'ul' outside the loop upon which to append the 'li's
 
   // hint: get 'The-Shire' by using its id
