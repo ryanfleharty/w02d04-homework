21ad95c870adc9307ef974a4eeaaa90dd67f6781
Alexandra Thornton <alexandrathornton@Alexandras-MacBook-Pro.local>
2019-03-28 19:35:05 -0600

    Chapter 12 complete - Gollum is trying to get the ring

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -261,10 +261,13 @@ const itsDangerousToGoAlone = () => {
 const weWantsIt = () => {
 
   // 1. Create a div with an id of 'gollum' and add it to Mordor
+  $('#Mordor').append('<div id=gollum></div>');
 
   // 2. Move the ring from Frodo and give it to Gollum
+  $('#the-ring').appendTo('#gollum');
 
   // 3. Move Gollum into Mount Doom
+  $('#gollum').appendTo('#mount-doom');
 
 };
 
