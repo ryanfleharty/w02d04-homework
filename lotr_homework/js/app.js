e8ad99653552758760c089f6d74df2cfd1f7ea6e
WillyD <sopwill3@gmail.com>
2019-03-28 20:32:30 -0600

    I will take the ring to Mordo

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -90,9 +90,14 @@ const makeHobbits = () => {
 const keepItSecretKeepItSafe = () => {
 
   // 1. create an empty div with an id of 'the-ring'
-
   // 2. add the ring as a child of Frodo
-
+  $('.hobbit').each(function(){
+    console.log(this.innerText);
+  if (this.innerText === "frodo baggins") {
+    console.log("Inside Frodo!!");
+    $(this).append("<div id='the-ring'></div>");
+  }
+  })
   // hint: Frodo does not have an id, but there is a command to retrieve all elements with a certain class. This should give you an array for you to access . . .
 
   // when you think you have given Frodo the ring, check in your Elements tab
