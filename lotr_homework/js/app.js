37648f7d3ca525de3926295aeeb70f9b49978792
Sami Austin <samicaustin@gmail.com>
2019-03-28 21:59:23 -0600

    Chapter 9 - Updated Gandalf

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -55,7 +55,6 @@ const makeMiddleEarth = () => {
 // ============
 
 const makeHobbits = () => {
-  console.log('Make hobbits');
   $(`#The-Shire`).append(`<ul class = "hobbit"/>`);
   for (i = 0; i < hobbits.length; i++){
     $(`ul`).append(`<li class= "hobbit">${hobbits[i]}</li>`);
@@ -141,13 +140,7 @@ const forgeTheFellowShip = () => {
 // Chapter 9
 // ============
 const theBalrog = () => {
-
-  // 1. change the 'Gandalf' textNode to 'Gandalf the White'
-
-  // 2. add a class "the-white" to this element
-
-  // 3. in the style.css file, add a css rule to make elements of the class "the-white" have a white background and a grey border
-
+  $('li.buddy').eq(0).replaceWith(`<li class="the-white">Gandalf the White</li>`);
 };
 
 // COMMIT YOUR WORK
