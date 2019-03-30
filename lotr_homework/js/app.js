aff520b70694b6971385b89439ee8de8fb675236
Nick Johnson <Blackjacknick@gmail.com>
2019-03-30 11:51:07 -0600

    Chapter 8 complete - The Fellowship is created

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -185,15 +185,17 @@ beautifulStranger();
 const forgeTheFellowShip = () => {
 
   // 1. create a new div with an id 'the-fellowship'
+  $('<div id="theFellowship"><h1>"The Fellowship"</h1></div>').appendTo("#middle-earth");
 
   // 2. add an h1 with the text 'The Fellowship' to this new div
 
   // 3. append the fellowship to middle-earth
 
   // 4. add the unordered lists of hobbits and buddies to 'the-fellowship'
-
+  $('.hobbit').appendTo('#theFellowship');
+  $('#fellowship').appendTo('#theFellowship');
 };
-
+forgeTheFellowShip();
 // COMMIT YOUR WORK
 // The commit message should read: "Chapter 8 complete - The Fellowship is created"
 
