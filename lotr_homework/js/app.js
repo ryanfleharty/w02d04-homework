3cd8f803190738575e8738fd174166eaf126e62d
Joe Luchini <joeluchini@Joes-MacBook-Pro.local>
2019-04-06 10:44:59 -0600

    chapter 11 complete

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -235,9 +235,11 @@ const hornOfGondor = () => {
 const itsDangerousToGoAlone = () => {
 
   // 1. take Frodo and Sam out of the fellowship and move them to Mordor (they don't need to be inside a ul in Mordor)
+    $(".hobbits").eq(0).detach().appendTo('#Mordor');
+    $(".hobbits").eq(1).detach().appendTo('#Mordor');
 
   // 2. add a div with an id of 'mount-doom' to Mordor
-
+  $('#Mordor').append('<div id="mount-doom"></div>')
 };
 
 // COMMIT YOUR WORK
